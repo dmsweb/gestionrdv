b3373f01756de6b099fbc44b6ace734f6047fbfd
aris <dovi.aristide@gmail.com>
2019-10-15 23:49:04 +0000

    Ajout du fichier de la base de donnee

---
+++ rdv.sql
@@ -0,0 +1,245 @@
+-- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: rdv
+-- ------------------------------------------------------
+-- Server version	5.7.27-0ubuntu0.19.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `domaine`
+--
+
+DROP TABLE IF EXISTS `domaine`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `domaine` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(50) NOT NULL,
+  `id_service` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `domaine_service_FK` (`id_service`),
+  CONSTRAINT `domaine_service_FK` FOREIGN KEY (`id_service`) REFERENCES `service` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `domaine`
+--
+
+LOCK TABLES `domaine` WRITE;
+/*!40000 ALTER TABLE `domaine` DISABLE KEYS */;
+INSERT INTO `domaine` VALUES (1,'administation',1),(2,'secrétariat',2);
+/*!40000 ALTER TABLE `domaine` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employer`
+--
+
+DROP TABLE IF EXISTS `employer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nomComplet` varchar(250) NOT NULL,
+  `telephone` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `adresse` varchar(50) NOT NULL,
+  `id_type_employer` int(11) NOT NULL,
+  `id_domaine` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `employer_type_employer_FK` (`id_type_employer`),
+  KEY `employer_domaine0_FK` (`id_domaine`),
+  CONSTRAINT `employer_domaine0_FK` FOREIGN KEY (`id_domaine`) REFERENCES `domaine` (`id`),
+  CONSTRAINT `employer_type_employer_FK` FOREIGN KEY (`id_type_employer`) REFERENCES `typeEmployer` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employer`
+--
+
+LOCK TABLES `employer` WRITE;
+/*!40000 ALTER TABLE `employer` DISABLE KEYS */;
+INSERT INTO `employer` VALUES (1,'Dovi Aristide','778580286','dovi.aristide@gmail.com','aristide','Libertï¿½ 5 villa 5508',1,1),(2,'Fatou thiam','776040467','fatou@gmail.com','thiam','golf',2,2),(3,'Rama ba','775689230','rama@gmail.com','rama','pc',3,2);
+/*!40000 ALTER TABLE `employer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `etat_rendez_vous`
+--
+
+DROP TABLE IF EXISTS `etat_rendez_vous`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `etat_rendez_vous` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `etat_rendez_vous`
+--
+
+LOCK TABLES `etat_rendez_vous` WRITE;
+/*!40000 ALTER TABLE `etat_rendez_vous` DISABLE KEYS */;
+/*!40000 ALTER TABLE `etat_rendez_vous` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `patient`
+--
+
+DROP TABLE IF EXISTS `patient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patient` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nomComplet` varchar(250) NOT NULL,
+  `adresse` varchar(50) NOT NULL,
+  `telephone` varchar(50) NOT NULL,
+  `motif` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patient`
+--
+
+LOCK TABLES `patient` WRITE;
+/*!40000 ALTER TABLE `patient` DISABLE KEYS */;
+/*!40000 ALTER TABLE `patient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `planning`
+--
+
+DROP TABLE IF EXISTS `planning`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `planning` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datePlanning` date NOT NULL,
+  `heurePlanning` datetime NOT NULL,
+  `id_employer` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `planning_employer_FK` (`id_employer`),
+  CONSTRAINT `planning_employer_FK` FOREIGN KEY (`id_employer`) REFERENCES `employer` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `planning`
+--
+
+LOCK TABLES `planning` WRITE;
+/*!40000 ALTER TABLE `planning` DISABLE KEYS */;
+/*!40000 ALTER TABLE `planning` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rendez_vous`
+--
+
+DROP TABLE IF EXISTS `rendez_vous`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rendez_vous` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dateRendezVous` date NOT NULL,
+  `heureRendezVous` datetime NOT NULL,
+  `id_patient` int(11) NOT NULL,
+  `id_employer` int(11) NOT NULL,
+  `id_etat_rendez_vous` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `rendez_vous_patient_FK` (`id_patient`),
+  KEY `rendez_vous_employer0_FK` (`id_employer`),
+  KEY `rendez_vous_etat_rendez_vous1_FK` (`id_etat_rendez_vous`),
+  CONSTRAINT `rendez_vous_employer0_FK` FOREIGN KEY (`id_employer`) REFERENCES `employer` (`id`),
+  CONSTRAINT `rendez_vous_etat_rendez_vous1_FK` FOREIGN KEY (`id_etat_rendez_vous`) REFERENCES `etat_rendez_vous` (`id`),
+  CONSTRAINT `rendez_vous_patient_FK` FOREIGN KEY (`id_patient`) REFERENCES `patient` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rendez_vous`
+--
+
+LOCK TABLES `rendez_vous` WRITE;
+/*!40000 ALTER TABLE `rendez_vous` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rendez_vous` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `service`
+--
+
+DROP TABLE IF EXISTS `service`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `service` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `service`
+--
+
+LOCK TABLES `service` WRITE;
+/*!40000 ALTER TABLE `service` DISABLE KEYS */;
+INSERT INTO `service` VALUES (1,'secrétariat'),(2,'administation');
+/*!40000 ALTER TABLE `service` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `typeEmployer`
+--
+
+DROP TABLE IF EXISTS `typeEmployer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `typeEmployer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `typeEmployer`
+--
+
+LOCK TABLES `typeEmployer` WRITE;
+/*!40000 ALTER TABLE `typeEmployer` DISABLE KEYS */;
+INSERT INTO `typeEmployer` VALUES (1,'admin'),(2,'secretaire'),(3,'médecin ');
+/*!40000 ALTER TABLE `typeEmployer` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-15 23:47:30
